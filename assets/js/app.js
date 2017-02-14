a7b21a26075e46841219fdb19ea427e6a51c29fa
Todd Croak-Falen <toddcf@gmail.com>
2017-02-14 13:20:53 -0800

    Added function to determine which hint to display based on distance between click and actual target.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,40 @@
 // Get random number from 0 to size:
 var getRandomNumber = function(size) {
 	return Math.floor(Math.random() * size);
-}
+};
 
-var width 	= 650;
-var height 	= 576;
+// Use Pythagorean Theorem to calculate distance between click and target:
+var getDistance = function(event, target) {
+	var diffX = event.offsetX - target.x;
+	var diffY = event.offsetY - target.y;
+	return Math.sqrt((diffX * diffY) + (diffX * diffY));
+};
+
+// Determine which hint to give:
+var getDistanceHint = function(distance) {
+	if (distance < 10) {
+		return "Boiling hot!";
+	}
+	else if (distance < 20) {
+		return "Really hot!";
+	}
+	else if (distance < 40) {
+		return "Hot!";
+	}
+	else if (distance < 80) {
+		return "Warm . . .";
+	}
+	else if (distance < 160) {
+		return "Cold.";
+	}
+	else if (distance < 320) {
+		return "Really cold . . ";
+	}
+	else {
+		return "Freezing.";
+	}
+};
+
+var width 	= 651;
+var height 	= 577;
 var clicks	= 0;
